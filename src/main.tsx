90114dc9b83865c0ffa88487c91842ca882163c0
nhithanh1998 <nhithanhtranho@example.com>
2025-11-10 08:10:06 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,9 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./global.css";
 
-// Lấy giá trị basename từ biến môi trường, ví dụ VITE_ENVIRONMENT
-const basename = import.meta.env.VITE_ENVIRONMENT === "production"
-  ? "/Ngoc-Tieu-Cac-Admin"
-  : "/";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
  <React.StrictMode>
-    <HashRouter basename={basename}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>

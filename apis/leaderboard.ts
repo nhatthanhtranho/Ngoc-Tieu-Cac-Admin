50d927647880f079dbe3a645cf25ae680c00ceef
nhithanh1998 <nhithanhtranho@example.com>
2025-11-15 15:41:03 +0700

    update

--- apis/leaderboard.ts
+++ apis/leaderboard.ts
@@ -1,18 +1,17 @@
-import axios from "axios";
-import { getEndpoint } from ".";
+import { api } from ".";
 
 export async function getLeaderboard(
   type: string,
   setLeaderboardBooks: (bookSlugs: string[]) => void
 ) {
-  const res = await axios.get(getEndpoint(`trendings/${type}`));
+  const res = await api.get(`/trendings/${type}`);
   setLeaderboardBooks(res.data.books);
   return res.data;
 }
 
 export async function setLeaderboard(type: string, bookSlugs: string[]) {
-  return axios.post(getEndpoint(`trendings`), {
-    type,
+  return api.post(`/trendings`, {
     books: bookSlugs,
+    type,
   });
 }

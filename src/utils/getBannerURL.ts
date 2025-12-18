2d2a43f34276196626e6fb1ee208e9ae3697c643
nhithanh1998 <nhithanhtranho@example.com>
2025-12-19 04:25:19 +0700

    update

--- src/utils/getBannerURL.ts
+++ src/utils/getBannerURL.ts
@@ -1,13 +1,13 @@
 const STORAGE = "https://ngoc-tieu-cac-public.s3.ap-southeast-1.amazonaws.com";
 
 export function getBannerURL(storySlug: string, type?: string): string {
-  if(type === "ngang") return `${STORAGE}/${storySlug}/banner-ngang.webp`;
+  if(type === "ngang") return `${STORAGE}/book-cover/${storySlug}/banner-ngang.webp`;
 
-  return `${STORAGE}/${storySlug}/banner.webp`;
+  return `${STORAGE}/book-cover/${storySlug}/banner.webp`;
 }
 
 export function getSmallBannerURL(storySlug: string): string {
-  return `${STORAGE}/${storySlug}/banner-small.webp`;
+  return `${STORAGE}/book-cover/${storySlug}/banner-small.webp`;
 }
 
 export function getAvatarUrl(id: string): string {
